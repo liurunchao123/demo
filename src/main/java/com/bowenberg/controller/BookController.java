d399384b04e9a055558b3c60700fca4a2ae4b66b
刘润超 <1143092249@qq.com>
2023-09-20 14:13:05 +0800

    first commit

---
+++ src/main/java/com/bowenberg/controller/BookController.java
@@ -0,0 +1,15 @@
+package com.bowenberg.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+public class BookController {
+    @GetMapping
+    public String getById(){
+        System.out.println("项目yyou运行了一次");
+        return "项目运行了一次";
+    }
+}
---
+++ src/test/java/com/bowenberg/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bowenberg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
